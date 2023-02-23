069aba13b258bf656c5ba2e4c0adfb2320f80f61
GeneralPlayfix <81032503+GeneralPlayfix@users.noreply.github.com>
2023-02-23 15:45:30 +0100

    reservation system update, add some verification

--- gestion_reservation_aviation/app_gestion_reservations/views.py
+++ gestion_reservation_aviation/app_gestion_reservations/views.py
@@ -9,12 +9,10 @@ def index(request):
     return HttpResponse("Hello World !")
 
 def school(request, id):
-    #TODO get user
-    
+    #TODO get user from cookie
     user_id = 1
     if User.objects.filter(id=user_id).exists() == False:
         return render(request, 'school.html', {'error':"User invalide"})
-    #TODO get user_object
 
     #TODO TEMP
     user = User.objects.get(id=user_id)
@@ -26,22 +24,33 @@ def school(request, id):
     if Cour.objects.filter(id_ecole_id=id).exists() == False:
         return render(request, 'school.html', {'error':"Aucune école n'a cet id"}) 
     cours = Cour.objects.filter(id_ecole_id=id)
-    cour_choices = [(c.id, c.cour_nom) for c in cours]
+    #TODO if possible, don't print already taken course
+    cour_choices = []
+    for c in cours:
+        if Reservation.objects.filter(id_cour_id=c.id, id_user_id=user.id).exists():
+            continue
+        cour_choices.append((c.id, c.cour_nom))
+            
     #check if there is some course, if not return error
     if len(cour_choices) <= 0:
-        return render(request, 'school.html', {'error':"Aucun cours pour cette école"})
+        return render(request, 'school.html', {'error':"Aucun cours pour cette école ou vous vous êtes déjà inscrit à tous les cours"})
 
     if request.method == ('POST'):
+
         form = ReservationForm(request.POST, cour_choices=cour_choices,user_id=user_id)
-        
+
         if form.is_valid():
             cour = Cour.objects.get(id=form.cleaned_data['id_cour'])
+            if Reservation.objects.filter(id_cour=cour.id, id_user=user.id).exists():
+                form2 = ReservationForm(cour_choices=cour_choices, user_id=user_id)  
+                return render(request, 'school.html', {'form':form2,'error':"Vous ne pouvez pas faire deux fois la meme réservation"})
+
             reservation = Reservation.objects.create(
                 reservation_date=form.cleaned_data['reservation_date'],
                 id_user=user,
                 id_cour=cour,
             )
-        return render(request, 'school.html', {'success':"Réservation réalisée"})
+        return render(request, 'school.html', {'success':"Réservation réalisée avec succès"})
     else:
         form = ReservationForm(cour_choices=cour_choices, user_id=user_id)  
         return render(request, 'school.html', {'form': form})

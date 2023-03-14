f74fa609d4a8a1de09ed19e424cfc6eca88dafa2
romain callewaert <84373640+zuwui@users.noreply.github.com>
2023-03-14 15:21:22 +0100

    [accueil] add: add accueil and reservation page

--- gestion_reservation_aviation/app_gestion_reservations/urls.py
+++ gestion_reservation_aviation/app_gestion_reservations/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="Login"),
+    path('accueil/', views.accueil, name="Accueil"),
+    path('reservation/', views.reservation, name="Reservation"),
 ]
--- gestion_reservation_aviation/app_gestion_reservations/views.py
+++ gestion_reservation_aviation/app_gestion_reservations/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World !")
+    return render('login')
 
+def accueil(request):
+    ecoles = Ecole.objects.all()
+    cours = Cour.objects.all()
+    return render(request, 'accueil/accueil.html', {'ecoles': ecoles, 'cours': cours })
+
+def reservation(request):
+    reservations = Reservation.objects.all()
+    ecoles = Ecole.objects.all()
+    cours = Cour.objects.all()
+    return render(request, 'reservation/reservation.html', {'reservations': reservations, 'cours': cours, 'ecoles': ecoles})
